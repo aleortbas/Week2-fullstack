a3067cbd40150cc5f94e78fd31cb7486cc2e3b23
Alejandro Ortiz <aleortbas@gmail.com>
2021-05-22 16:29:50 -0500

    NPM Scripts part 1

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('#login-link').on('click',function () {
+        $('#loginModal').modal();
+    })
+    $('#reserve-button').on('click',function () {
+        $('#ReserveModal').modal();
+    })
+    $('#mycarousel').carousel({ interval: 2000 });
+    $('#carouselButton').click(function () {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });      
+});
